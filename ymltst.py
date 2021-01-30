8d76c9b2408ff36694b8a8033a5a51c5dacd213e
David Funni <dfunni@gmail.com>
2021-01-29 16:46:14 -0800

    removed ymltst.py

--- ymltst.py
+++
@@ -1,6 +0,0 @@
-import yaml
-with open("config.yml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader).get('DEFAULT', 'DEFAULT')
-    print(type(cfg['pOT1']))
-    print(cfg['pOT1'])
-
