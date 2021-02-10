3c2ee0a592f097f91bfd017f0100d6915d5a8fb4
David Funni <dfunni@gmail.com>
2021-02-09 18:44:35 -0800

    changed to start scipts from /home/pi

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-source /home/pi/Documents/projects/TCx/env/bin/activate
+source $HOME/TCx/env/bin/activate
 (sudo socat pty,rawer,echo=0,link=/dev/ttyS90,group=dialout pty,rawer,echo=0,link=/dev/ttyS91,group=dialout) &
 PTYs="readlink -f /dev/ttyS90"
 PTYc="readlink -f /dev/ttyS91"
 sleep 1
 sudo chmod 660 $PTYs
 sudo chmod 660 $PTYc 
-#sudo pigpiod -s 10
-artisan &
-python /home/pi/Documents/projects/TCx/TCx.py
+# sudo pigpiod -s 10
+# artisan &
+python $HOME/TCx/TCx.py
 
--- test/start_test.sh
+++ test/start_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-source /home/pi/Documents/projects/TCx/env/bin/activate
+source $HOME/TCx/env/bin/activate
 (sudo socat pty,rawer,echo=0,link=/dev/ttyS90,group=dialout pty,rawer,echo=0,link=/dev/ttyS91,group=dialout) &
 PTYs="readlink -f /dev/ttyS90"
 PTYc="readlink -f /dev/ttyS91"
 sleep 1
 sudo chmod 660 $PTYs
 sudo chmod 660 $PTYc 
-python /home/pi/Documents/projects/TCx/TCx.py
+python $HOME/TCx/TCx.py
 
