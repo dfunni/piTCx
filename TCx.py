76a54adfb55aa49106e7d23b6b5dc37e246a727c
David Funni <dfunni@gmail.com>
2023-12-16 16:14:37 -0800

    docs cleanup

--- TCx.py
+++ TCx.py
@@ -1,4 +1,3 @@
-import os
 import logging
 import sys
 import serial
@@ -201,10 +200,6 @@ class TCx(object):
 
 
 if __name__ == '__main__':
-    # prio = os.sched_get_priority_max(os.SCHED_FIFO)
-    # param = os.sched_param(prio)
-    # os.sched_setscheduler(0, os.SCHED_FIFO, param)
-    # os.sched_setaffinity(0, {0})
     with open("config.yml", 'r', encoding="utf-8") as ymlfile:
         config = yaml.load(ymlfile, Loader=yaml.FullLoader)
     logging.basicConfig(filename=config['LOG_FILE'],
